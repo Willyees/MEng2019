3759d0e7820012a571226e11303b2647cc277235
Dylan Tyrie-Dron <dman9887@gmail.com>
2020-01-15 14:43:41 +0000

    home page textboxes and buttons added (no functionality yet) layout still needs tweaking

--- src/views/home.js
+++ src/views/home.js
@@ -1,11 +1,41 @@
 import React, {Component} from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+
 
 class HomeTemplate extends Component {
+
     render() {
         return(
             <div className="home">
+            <Container maxWidth="sm">    
                 Home Page
-            </div>
+                <TextField
+                fullWidth
+                    id="txt-user-name"
+                    label="Username"
+                    value=""
+                />
+        
+                <TextField
+                fullWidth
+                    id="txt-password"
+                    label="Password"
+                    value=""
+                />                     
+        
+                <Button
+                    id="btn-sign-in" variant="contained" color="primary" margin="normal" disableElevation >
+                    Sign In
+                </Button>
+        
+                <Button
+                    id="btn-sign-up" variant="contained" color="secondary" margin="normal" disableElevation >
+                    Sign Up
+                </Button>
+            </Container>
+        </div>
         );
     }
 }
