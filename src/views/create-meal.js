fd8e094ec65bc9827cd07b7a086e176341aa7c95
Alessio Williams Gava <marveel@hotmail.it>
2020-01-16 17:14:09 +0000

    added debug button that inputs all the fields with something, fixed bug that would not retreive the right state into the input value

--- src/views/create-meal.js
+++ src/views/create-meal.js
@@ -36,28 +36,45 @@ class CreateMealTemplate extends Component { //this is more create meal. have to
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
         this.handleDate = this.handleDate.bind(this);
+        this.debugFillFields = this.debugFillFields.bind(this);
     }
     
     handleDate(date_){ //date is handled differently (not like an event)
+        console.log("handle data");
         this.setState({date : date_});
         console.log(this.state.date + "state");
     }
 
     onChange(event){//every time an element is modified from the user this function is called. So it is possible to perform checks for each keystroke if needed
+        console.log("on change");
+        console.log(this.state.date([event.target.name]));
         console.log(event.target.value);
-        this.setState({[event.target.name] : event.target.value});
+        //this.setState({[event.target.name] : event.target.value});
     }
 
     onSubmit(event) {
+        console.log("on submit");
         //add to this fucntion the connection to the DB. can retreive all the inputs values from 'this.state'. care: they are stored all as strings at the moment (JS dynamic types)
         const {title, description, city} = this.state;
         console.log(this.state);
         event.preventDefault();
     }
 
-    render() { //todo: add id to all the inputs
+    //DEBUG ONLY
+
+    debugFillFields() {
+        console.log("debug full fields");
+        this.setState({title: "new Meal", description : "this is an informal meal to get to know new people that would like to be eaten", city : "Edinburgh",
+        dietary : "none", date : new Date().toDateString(), time : new Date().toLocaleTimeString(), proposed_meal : "make your own favorite pizza", expected_contribution : "4.5", guest_limit : "4", 
+        age_range: "none", suggested_theme : "none"});
+    }
+
+    render() {
         return(
             <div>
+                <Button variant="contained" color="secondary" onClick={this.debugFillFields}>
+                    Debug fill fields
+                </Button>
                 Create a Meal Event
             <div>
             <p />
@@ -65,20 +82,20 @@ class CreateMealTemplate extends Component { //this is more create meal. have to
             <form onSubmit={this.onSubmit}> 
                 <div>
                     {/* id: <name_id>_cm; cm stands for create meal */}
-                    <TextField name="title" id="title_cm" onChange={this.onChange} value={this.title} type="text" 
+                    <TextField name="title" id="title_cm" onChange={this.onChange} value={this.state.title} type="text" 
                     label="Title"/>
                 </div>
                 <p />
                 <div>
-                    <TextField multiline name="description" id="description_cm" onChange={this.onChange} value={this.description} type="text"
+                    <TextField multiline name="description" id="description_cm" onChange={this.onChange} value={this.state.description} type="text"
                     label="Description" variant="outlined"/>
                 </div>
                 <div>
-                    <TextField name="city" id="city_cm" onChange={this.onChange} value={this.city} type="text" label="City" 
+                    <TextField name="city" id="city_cm" onChange={this.onChange} value={this.state.city} type="text" label="City" 
                     />
                 </div>
                 <div>
-                    <TextField name="dietary" id="dietary_cm" onChange={this.onChange} value={this.dietary} label="Dietary requirements"/> {/*at the moment is a text box. Once DB connection is set up, should retreive multiple choices from DB and use a <select />*/}
+                    <TextField name="dietary" id="dietary_cm" onChange={this.onChange} value={this.state.dietary} label="Dietary requirements"/> {/*at the moment is a text box. Once DB connection is set up, should retreive multiple choices from DB and use a <select />*/}
                 </div>
                 <div>
                     <MuiPickersUtilsProvider utils={DateFnsUtils}>
@@ -87,22 +104,22 @@ class CreateMealTemplate extends Component { //this is more create meal. have to
                     </MuiPickersUtilsProvider>
                 </div>
                 <div>
-                    <TextField name="time" id="time_cm" onChange={this.onChange} value={this.time} type="time" label="Time"/>
+                    <TextField name="time" id="time_cm" onChange={this.onChange} value={this.state.time} type="time" label="Time"/>
                 </div>
                 <div>
-                    <TextField name="proposed_meal" id="proposed_meal_cm" onChange={this.onChange} value={this.proposed_meal} type="text" label="Proposed meal" />
+                    <TextField name="proposed_meal" id="proposed_meal_cm" onChange={this.onChange} value={this.state.proposed_meal} type="text" label="Proposed meal" />
                 </div>
                 <div>
-                    <TextField name="expected_contribution" id="expected_contribution_cm" onChange={this.onChange} value={this.expected_contribution} type="number" label="Expected contribution" />
+                    <TextField name="expected_contribution" id="expected_contribution_cm" onChange={this.onChange} value={this.state.expected_contribution} type="number" label="Expected contribution" />
                 </div>
                 <div>
-                    <TextField name="guest_limit" id="gues_limit_cm" onChange={this.onChange} value={this.guest_limit} type="number" label="Guest Limit" min="1"/>
+                    <TextField name="guest_limit" id="gues_limit_cm" onChange={this.onChange} value={this.state.guest_limit} type="number" label="Guest Limit" min="1"/>
                 </div>
                 <div>
-                    <TextField name="age_range" id="age_range_cm" onChange={this.onChange} value={this.age_range} type="range" label="Age range" min="0" max="99"/>
+                    <TextField name="age_range" id="age_range_cm" onChange={this.onChange} value={this.state.age_range} type="range" label="Age range" min="0" max="99"/>
                 </div>
                 <div>
-                    <TextField name="suggested_theme" id="suggested_theme_cm" onChange={this.onChange} value={this.age_range} type="text" label="Suggested Theme" />
+                    <TextField name="suggested_theme" id="suggested_theme_cm" onChange={this.onChange} value={this.state.age_range} type="text" label="Suggested Theme" />
                 </div>
                 <div>
 
