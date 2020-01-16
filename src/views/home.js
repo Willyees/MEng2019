27a297a6db506891396b06cec643631746b8efce
oscarmeanwell <oscar.meanwell@gmail.com>
2020-01-16 17:11:50 +0000

    Login/Sign Up working

--- src/views/home.js
+++ src/views/home.js
@@ -2,12 +2,47 @@ import React, {Component} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import $ from 'jquery';
 
-
+function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decC = decodeURIComponent(document.cookie);
+    var tmp = decC.split(';');
+    for(var i = 0; i <tmp.length; i++) {
+        var c = tmp[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 class HomeTemplate extends Component {
-
+    componentDidMount() {
+        var un = getCookie("temp");
+        if(typeof(un) != "undefined" & un !== null){
+             $("#txt-user-name").val(un);
+	}
+    }
     logUserIn(){
-	alert("It works");
+	 $.ajax({ url: 'login.php',
+            type: 'post',
+            data: {
+                "usr" : $("#txt-user-name").val(),
+	    	"pwd" : $("#txt-password").val()
+	    },
+            success: function(output) {
+                if(output == "DONE"){
+		    alert("LOGIN!");
+                }
+		else{
+		    alert("Invalid Credentials");
+		}
+            }
+        });
+
     }
 
     redirectSignUp(){
@@ -23,14 +58,12 @@ class HomeTemplate extends Component {
 		fullWidth
 			id="txt-user-name"
 			label="Username"
-			value=""
 		/>
 
 		<TextField
 		fullWidth
 			id="txt-password"
 			label="Password"
-			value=""
 		/>                     
 		<Button
 			id="btn-sign-in" onClick={this.logUserIn} variant="contained" color="primary" margin="normal" disableElevation >
