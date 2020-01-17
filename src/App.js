bc27b82c8b4177983dcef428a77597d1ac02c714
oscarmeanwell <oscar.meanwell@gmail.com>
2020-01-17 12:37:36 +0000

    whats goign on

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SignInTemplate from './views/sign-up.js';
 import LogInTemplate from './views/log-in.js';
 import CreateMealTemplate from './views/create-meal.js';
 import HomeTemplate from './views/home.js';
+import MapTemplate from './views/map.js';
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
@@ -20,6 +21,9 @@ function App() {
 
             <Route exact path="/log-in">
               < LogInTemplate />
+            </Route>
+            <Route exact path="/map">
+              < MapTemplate />
             </Route>
 
             <Route exact path="/create-meal">  {/* might have to modify the path, because each meal will have a different URL. Set up atm as work in progress*/}
