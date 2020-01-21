082ced64528d537795e10f93be423491fe655a95
Dylan Tyrie-Dron <dman9887@gmail.com>
2020-01-21 15:22:57 +0000

    finally added errors to the sign-up page- just username isnt checked on the fly

--- src/views/sign-up.js
+++ src/views/sign-up.js
@@ -13,61 +13,172 @@ import $ from 'jquery';
 
 import { makeStyles } from '@material-ui/core/styles';
 
+const postcodeRegex = RegExp(/^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$/);
+
+const phoneNumberRegex = RegExp(/^((\+44(\s\(0\)\s|\s0\s|\s)?)|0)7\d{3}(\s)?\d{6}$/g);
+
+//will check to see if everythings valid before sending data to db
+const formValid = formErrors => {
+    let valid = true;
+
+    Object.values(formErrors).forEach(val => {
+        val.length > 0 && (valid = false);
+    });
+    
+    return valid;
+};
+
 class SignUpTemplate extends Component {
     constructor(props){
         super(props);
         this.state = {
-            fName : "",
-            lName: "",
-            username: "",
-            password: "",
-            password_c: "",
-            address_1: "",
-            address_2: "",
-            postcode: "",
-            city: "",
-            dob:  new Date().toDateString(), //date and time will need to use a graphical calendar. At the moment are visualised for debug purposes
-            phone: "",
-            dietary: "",
-            bio: "",
-            profilePic: "",
-            allergens: "",
-        }
+            fName : null,
+            lName: null,
+            username: null,
+            password: null,
+            password_c: null,
+            address_1: null,
+            address_2: null,
+            postcode: null,
+            city: null,
+            country: null,
+            dob:  null, //date and time will need to use a graphical calendar. At the moment are visualised for debug purposes
+            phone: null,
+            dietary: null,
+            bio: null,
+            profilePic: null,
+            allergens: null,
 
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-        this.handleDOB = this.handleDOB.bind(this);
+            formErrors: {
+                fName : "",
+                lName: "",
+                username: "",
+                password: "",
+                password_c: "",
+                address_1: "",
+                postcode: "",
+                city: "",
+                country: "",
+                phone: "",
+            }
+        }
     }
     
-    handleDOB(date_){ //date is handled differently (not like an event)
-        this.setState({dob : date_});
-        console.log(this.state.dob + "state");
-    }
+    handleDOB = date_ => { //date is handled differently (not like an event)
+        let formErrors = this.state.formErrors;
+        formErrors.dob =
+                    date_.getFullYear() >= new Date().getFullYear()
+                    ? "dob can't be in current year"
+                    : "";
+        this.setState({formErrors, dob : date_}, () => console.log(this.state));
+    };
 
-    onChange(event){//every time an element is modified from the user this function is called. So it is possible to perform checks for each keystroke if needed
-        console.log(event.target.value);
-        this.setState({[event.target.name] : event.target.value});
-    }
-
-    onSubmit(event) {
-        //add to this fucntion the connection to the DB. can retreive all the inputs values from 'this.state'. care: they are stored all as strings at the moment (JS dynamic types)
-        const {fName, bio, city} = this.state;
-        console.log(this.state);
+    onChange = event => {//every time an element is modified from the user this function is called. So it is possible to perform checks for each keystroke if needed
         event.preventDefault();
-	$.ajax({ url: 'PHPF/signup.php',
-	    type: 'post',
-	    data: {"data" : this.state},
-	    success: function(output) {
-		if(output == "DONE"){
-	            window.location.href = "/" 
-		}
-		else if(output == 1){
-		    alert("Username Already Exists");
-		}
-	    }
-	});	
-    }
+        const { name, value} = event.target;
+        let formErrors = this.state.formErrors;
+
+        switch(name){
+            case 'fName':
+                formErrors.fName = 
+                    value.length < 3
+                    ? "minimum 3 characters required"
+                    : "";
+                break;
+            case 'lName':
+                formErrors.lName = 
+                    value.length < 3 
+                    ? "minimum 3 characters required"
+                    : "";
+                break;
+
+            //not sure how to test the username to see if its already in db so I have left the username check for now, should go here like the others though.
+
+            case 'password':
+                formErrors.password = 
+                    value.length < 6 
+                    ? "minimum 6 characters required"
+                    : "";
+                break;
+            case 'password_c':
+                formErrors.password_c = 
+                    value.match(this.state.password)
+                    ? ""
+                    : "passwords must match"
+                break;
+            case 'address_1':
+                formErrors.address_1 =
+                    value.length < 3 
+                    ? "minimum 3 characters required"
+                    : "";
+                break;
+
+            //address 2 not required as it is an optional field
+
+            case 'postcode':
+            formErrors.postcode = 
+                postcodeRegex.test(value)
+                ? ""
+                : "postcode should be a uk postcode";
+                break;
+            case 'city':
+                formErrors.city =
+                    value.length < 3 
+                    ? "minimum 3 characters required"
+                    : "";
+                break;
+            case 'country':
+                formErrors.country =
+                    value.length < 3 
+                    ? "minimum 3 characters required"
+                    : "";
+                break;
+            
+            //dob handled in dobhandler()
+
+            case 'phone':
+                formErrors.phone =
+                    phoneNumberRegex.test(value)
+                    ? ""
+                    : "phone number should match uk mobile number";
+                break;
+            
+            
+            default:
+            break;        
+        }
+
+        this.setState({formErrors, [name]: value }, () => console.log(this.state));
+
+    };
+
+    onSubmit = event => {
+        event.preventDefault();
+        if(formValid(this.state.formErrors)){
+            //add to this function the connection to the DB. can retreive all the inputs values from 'this.state'. care: they are stored all as strings at the moment (JS dynamic types)
+            const {fName, bio, city} = this.state;
+            //^dunno if other elements can be added to the line above (db) so i've just left it for now^
+            $.ajax({ url: 'PHPF/signup.php',
+                type: 'post',
+                data: {"data" : this.state},
+                success: function(output) {
+                    if(output == "DONE"){
+                            window.location.href = "/" 
+                    }
+                    else if(output == 1){
+                        alert("Username Already Exists");
+                    }
+                }
+            });
+        } else{
+            console.error('FORM INVALID - DISPLAY ERROR MESSAGE');
+        }
+        
+    };
+
+
     render() {
+        const { formErrors } = this.state;
         return(
             
             <div className="signup">
@@ -77,43 +188,43 @@ class SignUpTemplate extends Component {
                 </div>
                 <form onSubmit={this.onSubmit}> 
                     <div>
-                        <TextField name="fName" id="first-name-tb" onChange={this.onChange} value={this.fName} type="text" label="First Name"/>
+                        <TextField error= {formErrors.fName} helperText= {formErrors.fName} name="fName" id="first-name-tb" onChange={this.onChange} value={this.fName} type="text" label="First Name"/>
                     </div>
                     <div>
-                        <TextField name="lName" id="last-name-tb" onChange={this.onChange} value={this.lName} type="text" label="Last Name"/>
+                        <TextField error= {formErrors.lName} helperText= {formErrors.lName} name="lName" id="last-name-tb" onChange={this.onChange} value={this.lName} type="text" label="Last Name"/>
                     </div>
                     <div>
-                        <TextField name="username" id="username-tb" onChange={this.onChange} value={this.username} type="text" label="Username" />
+                        <TextField error= {formErrors.username} helperText= {formErrors.username} name="username" id="username-tb" onChange={this.onChange} value={this.username} type="text" label="Username" />
                     </div>
                     <div>
-                        <TextField name="password" type="password" id="password-tb" onChange={this.onChange} value={this.password} label="Password" />
+                        <TextField error= {formErrors.password} helperText= {formErrors.password} name="password" type="password" id="password-tb" onChange={this.onChange} value={this.password} label="Password" />
                     </div>
                     <div>
-                        <TextField name="password_c" type= "password" id="password_c-tb" onChange={this.onChange} value={this.password_c} label="Confirm Password" />
+                        <TextField error= {formErrors.password_c} helperText= {formErrors.password_c} name="password_c" type= "password" id="password_c-tb" onChange={this.onChange} value={this.password_c} label="Confirm Password" />
                     </div>
                     <div>
-                        <TextField name="address_1" id="address_1-tb" onChange={this.onChange} value={this.address_1} type="text" label="Address Line 1" />
+                        <TextField error= {formErrors.address_1} helperText= {formErrors.address_1} name="address_1" id="address_1-tb" onChange={this.onChange} value={this.address_1} type="text" label="Address Line 1" />
                     </div>
                     <div>
-                        <TextField name="address_2" id="address_2-tb" onChange={this.onChange} value={this.address_2} type="text" label="Address Line 2" />
+                        <TextField error= {formErrors.address_2} helperText= {formErrors.address_2} name="address_2" id="address_2-tb" onChange={this.onChange} value={this.address_2} type="text" label="Address Line 2" />
                     </div>
                     <div>
-                        <TextField name="postcode" id="postcode-tb" onChange={this.onChange} value={this.postcode} type="text" label="Postcode" />
+                        <TextField error= {formErrors.postcode} helperText= {formErrors.postcode} name="postcode" id="postcode-tb" onChange={this.onChange} value={this.postcode} type="text" label="Postcode" />
                     </div>
                     <div>
-                        <TextField name="city" id="city-tb" onChange={this.onChange} value={this.city} type="text" label="City" />
+                        <TextField error= {formErrors.city} helperText= {formErrors.city} name="city" id="city-tb" onChange={this.onChange} value={this.city} type="text" label="City" />
                     </div>
                     <div>
-                        <TextField name="country" id="country-tb" onChange={this.onChange} value={this.country} type="text" label="Country" />
+                        <TextField error= {formErrors.country} helperText= {formErrors.country} name="country" id="country-tb" onChange={this.onChange} value={this.country} type="text" label="Country" />
                     </div>
                     <div>
                         <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                            <KeyboardDatePicker name="dob" id="dobdate" margin="normal" clearable autoOk={true} disableOpenOnEnter variant="inline" label="Date of Birth" format="dd/MM/yyyy"
+                            <KeyboardDatePicker error= {formErrors.dob} helperText= {formErrors.dob} name="dob" id="dobdate" margin="normal" clearable autoOk={true} disableOpenOnEnter variant="inline" label="Date of Birth" format="dd/MM/yyyy"
                             value={this.state.dob} onChange={this.handleDOB} />
                         </MuiPickersUtilsProvider>
                     </div>
                     <div>
-                        <TextField name="phone" id="phone-tb" onChange={this.onChange} value={this.phone} type="text" label="Phone Number" />
+                        <TextField error= {formErrors.phone} helperText= {formErrors.phone} name="phone" id="phone-tb" onChange={this.onChange} value={this.phone} type="text" label="Phone Number" />
                     </div>
                     <div>
                         <TextField name="dietary" id="dietary-ddmenu" onChange={this.onChange} value={this.dietary} label="Dietary requirements"/> {/*at the moment is a text box. Once DB connection is set up, should retreive multiple choices from DB and use a <select />*/}
