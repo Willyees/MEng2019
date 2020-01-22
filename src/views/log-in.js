eaa0b327fae2bc31e8aad0d0d2f7b03d2247628a
Dylan Tyrie-Dron <dman9887@gmail.com>
2020-01-22 16:17:42 +0000

    AppBar on root page and login page replace home page

--- src/views/log-in.js
+++ src/views/log-in.js
@@ -1,11 +1,39 @@
 import React, {Component} from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 
 class LogInTemplate extends Component {
     render() {
         return(
-            <div className="log-in">
-                LogIn page
-            </div>
+            <div className="login">
+            <Container maxWidth="sm">    
+                login Page
+		<TextField
+		fullWidth
+			id="txt-user-name"
+			label="Username"
+		/>
+
+		<TextField
+		fullWidth
+			id="txt-password"
+			type="password"
+            label="Password"
+            onChange = {this.onChange}
+            onKeyUp = {this.onKeyUp}
+		/>                     
+		<Button
+			id="btn-sign-in" onClick={this.logUserIn} ref={this.setSubmitBtnRef} variant="contained" color="primary" margin="normal" disableElevation >
+			Sign In
+		</Button>
+        
+		<Button
+			id="btn-sign-up" onClick={this.redirectSignUp} variant="contained" color="secondary" margin="normal" disableElevation >
+            Sign Up
+		</Button>
+	    </Container>
+        </div>
         );
     }
 }
