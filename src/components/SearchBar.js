aefbb48b29d9b158dff8bdd771c88159c5ef7bfb
Alessio Williams Gava <marveel@hotmail.it>
2020-02-04 14:23:49 +0000

    added dietary type to the filter list. Only passing the non-empty inputs when user submit. problem: date cannot be cleared when set; time has to be implemented in a differetn way: can't ask the user to input a specific time for the meal want to find

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,6 +10,8 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography'
 import {MuiPickersUtilsProvider, KeyboardDatePicker, DatePicker} from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
 
 
 const styles = ({
@@ -45,21 +47,32 @@ class SearchBar extends Component {
         
         this.state = {
             values : {
-                title : null,
-                city : null,
-                date : null,
-                time : null,
-        },
+                title : "",
+                city : "",
+                date : "",
+                time : "",
+                dietary : "",
+            },
             vis : {
                filter_vis : false, 
+            },
+            formErrors : {
+                city : "",
             }
         }
-
+        this.dietary = [];
+        this.getDietReqDb();
         this.handleClickFilter = this.handleClickFilter.bind(this);
         this.handleDate = this.handleDate.bind(this);
         this.handleOnChange = this.handleOnChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
     }
+   
 
+    getDietReqDb(){
+        //ajax call to db to get the available dietary requirements. Or just hardcode them here
+        this.dietary = ['Vegetarian','Vegan'];
+    }
     //user is not forced to fill all the fields, can search using any of the fields. The only one needed is city
     //might be useful to Oscar when querying the DB
     getListFieldsSearch(){
@@ -76,9 +89,35 @@ class SearchBar extends Component {
         return v;
     }
 
+    //at least the city must be specified, otherwise error shown
+    handleSearch(event){
+        if(this.state.values.city == ""){
+            this.setState({...this.state, formErrors : { ...this.state.formErrors, city : "city must be chosen"}});
+            return;
+        }
+
+        //ajax call with this.state.values wiht only the non empty ones
+        let obj = {}
+        for(let v in this.state.values){
+            if(this.state.values[v] != ""){
+                obj[v] = this.state.values[v];
+            }
+        }
+    }
+
     handleOnChange(event){
-        this.setState({...this.state, values : { ...this.state.values, [event.target.name] : event.target.value}});
-        this.getListFieldsSearch();
+        if(event.target.name == "city"){
+            var error_text = "";
+            if(event.target.value.length == 0){
+                error_text = "city must be chosen";
+            }
+            this.setState({...this.state, formErrors : { ...this.state.formErrors, city : error_text}, values : {...this.state.values, city : event.target.value }});
+
+        }
+        else{
+            this.setState({...this.state, values : { ...this.state.values, [event.target.name] : event.target.value}});
+        }
+        console.log(this.state.values);
     }
 
     handleDate(date_){ //date is handled differently (not like an event)
@@ -104,13 +143,13 @@ class SearchBar extends Component {
         <div className={classes.root}>
             <Paper className={classes.paper} >
                 {/*<IconButton className={classes.iconButton} id="search_btn" type="submit"/ > */} <SearchIcon className={classes.iconSearch}/>
-                <TextField id="search-bar" name="title" placeholder="Title keywords" type="search" value={this.state.title} onChange={this.handleOnChange}></TextField>  
+                <TextField id="search-bar" name="title" placeholder="Title keywords" type="search" value={this.state.values.title} onChange={this.handleOnChange}></TextField>  
             </Paper>
             <Paper className={classes.paper} >
                 <LocationOnIcon className={classes.iconSearch} />
-                <TextField id="search-bar" name="city   " placeholder="City" type="search" value={this.state.city} onChange={this.handleOnChange}></TextField>  
+                <TextField error={this.state.formErrors.city} helperText={this.state.formErrors.city} id="search-bar" name="city" placeholder="City" type="search" value={this.state.values.city} onChange={this.handleOnChange}></TextField>  
             </Paper>
-            <Button id="search-button" className={classes.submitButton} variant="contained" color="primary" >
+            <Button id="search-button" className={classes.submitButton} variant="contained" color="primary" onClick={this.handleSearch}>
                 SEARCH
             </Button>
             <Typography className={classes.filtersA} variant="subtitle1"><a className={classes.filtersA} href="#" onClick={this.handleClickFilter}>More filters</a></Typography>
@@ -121,15 +160,23 @@ class SearchBar extends Component {
             <Paper className={classes.paper}>
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
                 <KeyboardDatePicker name="date" margin="normal" clearable autoOk={true} variant="inline" format="dd/MM/yyyy"
-                value={this.state.date} onChange={this.handleDate} />
+                value={this.state.values.date} onChange={this.handleDate} />
             </MuiPickersUtilsProvider>
             </Paper>
             <Paper className={classes.paper}>
-            <TextField name="time" id="time_cm" type="time" label="Time" value={this.state.time}/>
+            <TextField name="time" id="time_cm" type="time" label="Time" value={this.state.values.time} onChange={this.handleOnChange}/>
             </Paper>
             <Paper className={classes.paper}>
 
             </Paper>
+            <Paper className={classes.paper}>
+                <Select displayEmpty value={this.state.values.dietary} name="dietary" onChange={this.handleOnChange}>
+                    <MenuItem value="" disabled>Dietary Type</MenuItem>                    
+                    {this.dietary.map((v, k) => <MenuItem value={v} key={k}>{v}</MenuItem>)}
+                    <MenuItem value="none">None</MenuItem>
+                </Select>
+            </Paper>
+
             {/*<Button id="filter-button" className={classes.submitButton} variant="contained" color="primary">FILTER</Button> need filter button? */}
         </div>}
     </div>
