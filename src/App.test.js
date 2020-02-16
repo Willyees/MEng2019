c82fa03f467c665ba57ced9682b9a7d1ba18d840
Alessio Williams Gava <marveel@hotmail.it>
2020-02-16 13:01:49 +0000

    using premade tests with a cicleci orb. Set up a very simple test that will check inital page

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, getAllByText } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getAllByText } = render(<App />);
+  const linkElement = getAllByText(/Meal Time/i);//searching for the text Meal Time. (should be everywherer because is on the nav bar)
+  expect(linkElement[0]).toBeInTheDocument();
 });
