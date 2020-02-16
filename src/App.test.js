17c0b5dd95ada0a7bcea5dead446357a364cbd22
Alessio Williams Gava <marveel@hotmail.it>
2020-02-16 13:11:45 +0000

    had to to new commit to be able to run again on circleci

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, getAllByText } from '@testing-library/react';
 import App from './App';
 
+
 test('renders learn react link', () => {
   const { getAllByText } = render(<App />);
   const linkElement = getAllByText(/Meal Time/i);//searching for the text Meal Time. (should be everywherer because is on the nav bar)
