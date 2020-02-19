071a039eeac0cd5d9bc05e64ae8619173adbde78
Dylan Tyrie-Dron <dman9887@gmail.com>
2020-02-19 14:09:39 +0000

    sign up page complete bar a problem with the image resizing the rest of the page plus maybe some of the textfields might be spaced apart later on

--- src/views/sign-up.js
+++ src/views/sign-up.js
@@ -22,8 +22,12 @@ import SaveIcon from '@material-ui/icons/Save';
 import profile from "../res/profile.png";
 import Rating from '@material-ui/lab/Rating';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, rgbToHex } from '@material-ui/core/styles';
 import AppBar from '../components/AppBar.js';
+
+import board from "../res/chopping_board_chopped.png";
+
+import myStyle from "../mystyle.module.css";
 
 const postcodeRegex = RegExp(/^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$/);
 
@@ -45,7 +49,7 @@ const useStyles = makeStyles(theme => ({
       flexGrow: 1,
     },
     profilePaper: {
-      padding: theme.spacing(6),
+      padding: theme.spacing(10),
       // textAlign: 'center',
       color: theme.palette.text.secondary,
       
@@ -220,28 +224,71 @@ class SignUpTemplate extends Component {
         
     };
 
+    handleImageChange = event => {
+        event.preventDefault();
+    
+        let reader = new FileReader();
+        let file = event.target.files[0];
+    
+        reader.onloadend = () => {
+          this.setState({
+            file: file,
+            imagePreviewUrl: reader.result
+          });
+        }
+
+        if (event.target.files[0]) {reader.readAsDataURL(file);}
+      };
+
 
     render() {
         const { formErrors } = this.state;
         const classes = useStyles;
+
+        let {imagePreviewUrl} = this.state;
+        let $imagePreview = null;
+        //problem with the image size, I want it to remain the same dimensions but it always grows with the page...
+        if (imagePreviewUrl) {
+            $imagePreview = (
+                <div style={{maxHeight:'100%', maxWidth: '100%', height:'100%', width:'100%'}}>
+                    <img src={imagePreviewUrl} 
+                        className= {myStyle.image} style={{ contentFit:'contain',height: '100%', width:'100%', maxHeight:'100%', maxWidth:'100%',
+                        border: "1px solid #ddd", borderRadius: "15px", 
+                        }}
+                    />
+                </div>
+            );
+
+        } 
+        else {
+        $imagePreview = (
+                <div style={{maxHeight:'100%', maxWidth: '100%', height:'100%', width:'100%'}}>
+                    <img src={profile} height="100%" width="100%" className= {myStyle.image} style={{
+                        contentFit:'contain',border: "1px solid #ddd", borderRadius: "15px", 
+                        }} 
+                    />
+                </div>
+            )
+        }
+
         // const [ value, setValue] = React.useState(2);
         // const [ hover, setHover] = React.useState(-1);
         return(
 
             <div className={classes.root}>
                 <form onSubmit={this.onSubmit}>
-                <Grid container style={{width:'75%', marginLeft:'2.5%'}}>
+                <Grid container style={{width:'75%', marginLeft:'19%'}}>
                     {/* first half of page */}
-                    <Grid item container xs={6} style={{marginTop:'7.5%', maxWidth:'40%'}}>
+                    <Grid item container xs={6} style={{marginTop:'7.5%', maxWidth:'40%', marginBottom:'7.5%'}}>
                         {/* profile paper */}
                         
-                            <Paper className={classes.profilePaper}>
+                            <Paper className={classes.profilePaper} style={{padding:'5%', "background-image" : `url(${board})`}}>
                                 <Grid item container xs={12} style={{justifyContent: "center"}}>
                                     {/* these textfields should be hidden until edit button clicked */}
                                     {/* <TextField label="First Name" style={{fontWeight: "bolder", marginTop: "-8%"}} 
                                     /> */}
 
-                                    <TextField error= {formErrors.fName} helperText= {formErrors.fName} name="fName" id="first-name-tb" onChange={this.onChange} value={this.fName} type="text" label="First Name" style={{fontWeight: "bolder"}} />
+                                    <TextField error= {formErrors.fName} helperText= {formErrors.fName} name="fName" id="first-name-tb" onChange={this.onChange} value={this.fName} type="text" label="First Name" style={{fontWeight: "bolder", color: 'red'}} />
 
                                     {/* <TextField label="Last Name" style={{fontWeight: "bolder", marginTop: "-8%"}}/> */}
 
@@ -255,7 +302,40 @@ class SignUpTemplate extends Component {
 
                                 <Grid item container xs={12} >
                                     <Grid item xs={12} style={{justifyContent:"left", display: "flex"}} >
-                                        <img src={profile} height="400px" width="100%" style={{border: "1px solid #ddd", borderRadius: "15px"}}/>
+                                        <div className={ myStyle.container }>
+                                         {/* style={{width:"100%", position: 'relative'}} */}
+                                            {$imagePreview}
+                                                {/* // opacity: 1,
+                                                // display: 'block',
+                                                // transition: '.5s ease',
+                                                // backfaceVisibility: 'hidden', */}
+                                            
+                                             <div className= {myStyle.middle}>
+                                            {/*style={{
+                                                transition: '.5s ease',
+                                                opacity: 0,
+                                                position: 'absolute',
+                                                top: '50%',
+                                                left: '50%',
+                                                transform: 'translate(-50%, -50%)',
+                                                MsTransform: 'translate(-50%, -50%)',
+                                                textAlign: 'center'}} */}
+                                            
+                                                 <div className= {myStyle.text} >
+                                                {/*style={{
+                                                    backgroundColor: '#4CAF50',
+                                                    color: 'white',
+                                                    fontSize: '16px',
+                                                    padding: '16px 32px',}} */}
+                                                
+                                                    
+                                                    <input className="fileInput" 
+                                                    type="file"
+                                                    onChange={this.handleImageChange} />
+                                                </div>
+
+                                            </div>
+                                        </div>
                                     </Grid>
 
                                     {/* <Grid item xs={12}>
@@ -349,22 +429,13 @@ class SignUpTemplate extends Component {
 
                                         <TextField error= {formErrors.phone} helperText= {formErrors.phone} name="phone" id="phone-tb" onChange={this.onChange} value={this.phone} type="text" label="Phone Number" fullWidth/>
                                     </Grid>
-                                    <Grid item xs={12}>
-                                        <Button
-                                            variant="contained"
-                                            color="primary"
-                                            size="small"
-                                            className={classes.button}
-                                            startIcon={<SaveIcon />}
-                                            type="submit"
-                                        >
-                                            Save
-                                        </Button>
+                                    {/* <Grid item xs={12}>
+                                        
 
-                                        {/* <Button variant="contained" color="primary" startIcon={<AddIcon />} type="submit">
+                                         <Button variant="contained" color="primary" startIcon={<AddIcon />} type="submit">
                                             Sign Up
-                                        </Button> */}
-                                    </Grid>
+                                        </Button>
+                                    </Grid> */}
                                 </Grid>
                             </Paper>
                     </Grid>
@@ -372,8 +443,8 @@ class SignUpTemplate extends Component {
                     {/* second half of page */}
                     <Grid item container xs={6} style={{marginTop:'7.5%', marginLeft:'2.5%', maxWidth:'40%'}}>
                         {/* recent meals paper */}
-                        <Grid item container xs={12}>
-                            <Paper className={classes.recentMealsPaper}>
+                        <Grid item container xs={12} style={{maxHeight:'40%'}}>
+                            <Paper className={classes.recentMealsPaper} style={{padding:'5%',"background-image" : `url(${board})`, height: '100%',maxHeight: '100%',maxWidth:'90%'}}>
                                 <Grid item container xs={12}>
                                     <GridList />
                                 </Grid>
@@ -389,27 +460,48 @@ class SignUpTemplate extends Component {
                         </Grid>
                         
                         {/* preferences paper */}
-                        <Grid item container xs={12} style={{width:'40%'}}>
-                            <Paper className={classes.recentMealsPaper}>
-                                <Grid item xs={12}>
+                        <Grid item container xs={12} style={{width:'40%', marginTop:'7.5%'}}>
+                            <Paper style={{width:'100%', padding:'5%',marginBottom:'18.7%', "background-image" : `url(${board})`}}>
+                                
                                     {/* <TextField label="Dietry Requirements" fullWidth defaultValue="no cheese"
                                     /> */}
                                     
                                     <TextField name="dietary" id="dietary-ddmenu" onChange={this.onChange} value={this.dietary} label="Dietary requirements" fullWidth/>
-                                </Grid>
+                                
 
                                 <Grid item xs={12}>
                                     {/* <TextField label="Allergens" fullWidth defaultValue="dogs"
                                     /> */}
 
-                                    <TextField multiline name="allergens" id="allergens-mltb" onChange={this.onChange} value={this.allergens} type="text" label="Allergens" variant="outlined"/>
+                                    <TextField multiline name="allergens" id="allergens-mltb" onChange={this.onChange} value={this.allergens} type="text" label="Allergens" variant="outlined" fullWidth/>
                                 </Grid>
                             </Paper>
                         </Grid>
                         
                     </Grid>
-
+                    
                 </Grid>
+                {/* footer */}
+                
+                <div class='fixed' style={{width:'100%', position:'fixed', bottom:0,
+                left: 0,
+                backgroundColor: 'rgb(76, 175, 80, 0.6)',
+                color: 'white',
+                textAlign: 'center'}}>
+                    <Button
+                        variant="contained"
+                        size="medium"
+                        color= 'primary'
+                        className={classes.button}
+                        startIcon={<AddIcon />}
+                        type="submit"
+                        style={{margin:'0.5%'}}
+                        
+                    >
+                        Sign Up
+                    </Button>
+                </div>
+                
                 </form>
             </div>
             
